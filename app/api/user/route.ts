44ee0b94d7e347c9ecc6afac3eb468965b2a5125
MundoKaizen <mundo@kaizencorps.com>
2023-09-14 18:39:24 -0400

    Testing husky precommit

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,56 +1,46 @@
-import { NextRequest, NextResponse } from "next/server";
-import AccountsHandler, { IAccount } from "../../../db/modeling/account";
+import { NextRequest, NextResponse } from 'next/server'
+import AccountsHandler, { IAccount } from '../../../db/modeling/account'
 
 export async function HEAD(request: Request) {}
 
 export async function POST(request: Request) {
-  // await connectToDb();
+    // await connectToDb();
 
-  const { username, password, company } = await request.json();
+    const { username, password, company } = await request.json()
 
-  const account: IAccount | null = await AccountsHandler.create(
-    company,
-    username,
-    password,
-  );
+    const account: IAccount | null = await AccountsHandler.create(
+        company,
+        username,
+        password
+    )
 
-  return NextResponse.json(account);
+    return NextResponse.json(account)
 }
 
 export async function PUT(request: Request) {
-  const { id, address } = await request.json(); 
+    const { id, address } = await request.json()
 
-  const account: IAccount | null = await AccountsHandler.update(id, address);
+    const account: IAccount | null = await AccountsHandler.update(id, address)
 
-  return new Response(
-    JSON.stringify({
-      account,
-    }),
-  );
+    return new Response(
+        JSON.stringify({
+            account,
+        })
+    )
 }
 
 export async function DELETE(request: Request) {
-  const { id } = await request.json(); // todo make a req type for this???
+    const { id } = await request.json() // todo make a req type for this???
 
-  const account: IAccount | null = await AccountsHandler.deleteAccount(id);
+    const account: IAccount | null = await AccountsHandler.deleteAccount(id)
 
-  return new Response(
-
-
-    
-    JSON.stringify({
-      message: "Account: " + account?.username + " deleted",
-    }),
-  );
+    return new Response(
+        JSON.stringify({
+            message: 'Account: ' + account?.username + ' deleted',
+        })
+    )
 }
 
-export async function PATCH(request: Request) {
+export async function PATCH(request: Request) {}
 
-
-
-}
-
-export async function OPTIONS(request: Request) {
-
-
-}
+export async function OPTIONS(request: Request) {}
