947a2509b717767b8b1fd74fdb1572e041c1b29c
MundoKaizen <mundo@kaizencorps.com>
2023-10-26 22:48:10 -0500

    Changed method of authenticating with google cloud storage

--- app/(clients)/google/index.tsx
+++ app/(clients)/google/index.tsx
@@ -1,8 +1,23 @@
 import { Storage, StorageOptions } from '@google-cloud/storage';
 
+const credentials = {
+    type: 'service_account',
+    project_id: 'piecework-btc',
+    private_key_id: process.env.GCS_PRIVATE_KEY_ID,
+    private_key: process.env.GCS_PRIVATE_KEY,
+    client_email: 'tmp-921@piecework-btc.iam.gserviceaccount.com',
+    client_id: '116488633130304170253',
+    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+    token_uri: 'https://oauth2.googleapis.com/token',
+    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+    client_x509_cert_url:
+        'https://www.googleapis.com/robot/v1/metadata/x509/tmp-921%40piecework-btc.iam.gserviceaccount.com',
+    universe_domain: 'googleapis.com',
+};
+
 const options: StorageOptions = {
     projectId: process.env.GCS_PROJECT_ID,
-    keyFilename: process.env.GCS_FILE_PATH,
+    credentials,
 };
 
 const storage = new Storage(options);
