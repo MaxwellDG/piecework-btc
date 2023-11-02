1707927c1cfc9ea6a63dc34071a341e8d189275c
MundoKaizen <mundo@kaizencorps.com>
2023-11-02 14:46:58 -0500

    Linting for vercel

--- app/dashboard/settings/howitworks/page.tsx
+++ app/dashboard/settings/howitworks/page.tsx
@@ -51,10 +51,10 @@ export default function Page() {
                                 Pending actions:&nbsp;
                             </p>
                             <p>
-                                Anything that requires a response from you in
+                                {`Anything that requires a response from you in
                                 regards to a task while be posted here to notify
                                 you. You can click on these to be navigated
-                                directly to what needs to be addressed.
+                                directly to what needs to be addressed.`}
                             </p>
                         </span>
                     </li>
@@ -77,8 +77,8 @@ export default function Page() {
                         <span className="flex">
                             <p className="font-semibold">Tasks:&nbsp;</p>
                             <p>
-                                Click on a project to go to the 'Tasks' screen.
-                                Here you can create specific tasks{' '}
+                                {`Click on a project to go to the 'Tasks' screen.
+                                Here you can create specific tasks{' '}`}
                             </p>
                         </span>
                     </li>
@@ -88,12 +88,12 @@ export default function Page() {
                                 Task Status:&nbsp;
                             </p>
                             <p>
-                                A Piecework-BTC team member will either be
+                                {`A Piecework-BTC team member will either be
                                 assigned to your task, or will contact you about
                                 negotiating the price. If a price is agreed
                                 upon, the team member will begin working on the
                                 project. Make sure to revisit this page often,
-                                as the status of your task will be updated here.
+                                as the status of your task will be updated here.`}
                             </p>
                         </span>
                     </li>
@@ -101,12 +101,12 @@ export default function Page() {
                         <span className="flex">
                             <p className="font-semibold">Payment:&nbsp;</p>
                             <p>
-                                When both parties agree a task is completed, a
+                                {`When both parties agree a task is completed, a
                                 BTC address will be shown on the respective task
                                 screen. Payment must be made here in order to
                                 continue adding future tasks. As successful
                                 payments are made overtime, the maximum amount
-                                of tasks alotted will be increased
+                                of tasks alotted will be increased`}
                             </p>
                         </span>
                     </li>
@@ -116,8 +116,8 @@ export default function Page() {
                     <h2 className="font-bold ml-2 text-3xl">Messages</h2>
                 </div>
                 <p>
-                    Communicate directly with the Piecework-BTC team about
-                    anything you'd like.
+                    {`Communicate directly with the Piecework-BTC team about
+                    anything you'd like.`}
                 </p>
                 <p className="mb-8">
                     Responses will be sent in less than 24 hours.
@@ -135,7 +135,7 @@ export default function Page() {
                             <p className="font-semibold whitespace-nowrap">
                                 Account settings:&nbsp;
                             </p>
-                            <p>Update your account's username and password</p>
+                            <p>{`Update your account's username and password`}</p>
                         </span>
                     </li>
                     <li>
@@ -143,7 +143,7 @@ export default function Page() {
                             <p className="font-semibold whitespace-nowrap">
                                 Company settings:&nbsp;
                             </p>
-                            <p>View, add, remove users from your company.</p>
+                            <p>{`View, add, remove users from your company.`}</p>
                         </span>
                     </li>
                     <li>
@@ -151,7 +151,7 @@ export default function Page() {
                             <p className="font-semibold whitespace-nowrap">
                                 How it works:&nbsp;
                             </p>
-                            <p>Hi. That's where you are right now</p>
+                            <p>{`Hi. That's where you are right now`}</p>
                         </span>
                     </li>
                 </ul>
